dcd13fa3f14b83a9a7ab5f3819a9fe31297d9fd4
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2023-02-14 13:25:05 +0100

    tme4

---
+++ tme-04/iads/utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package: iads
+File: utils.py
+Année: LU3IN026 - semestre 2 - 2022-2023, Sorbonne Université
+"""
+
+
+# Fonctions utiles pour les TDTME de LU3IN026
+# Version de départ : Février 2023
+
+# import externe
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# ------------------------ 
