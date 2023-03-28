a5632751ce55ef17292648479fe818c01b2e6c80
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2023-03-28 13:48:20 +0200

    fin 6 + presque fin 7

--- iads/Classifiers.py
+++ iads/Classifiers.py
@@ -165,7 +165,7 @@ class ClassifierKNN_MC(Classifier):
 
     def score(self, x):
         distance = np.argsort(np.linalg.norm(self.desc - x, axis=1))
-        self.label[distance[:self.k]]
+        knn = self.label[distance[:self.k]]
         return collections.Counter(knn).most_common(1)[0][0]
 
     def predict(self, x):
