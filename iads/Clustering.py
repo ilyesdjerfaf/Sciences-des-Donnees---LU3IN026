545eb7207dd9046a9cf86c5b27b1e04260e837fb
AnyesTAFOUGHALT <tafoughaltanyes@gmail.com>
2023-04-15 22:21:30 +0200

    tme 9 vf

--- iads/Clustering.py
+++ iads/Clustering.py
@@ -17,6 +17,9 @@ import scipy.cluster.hierarchy
 import math
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
+import matplotlib.cm as cm
+
+import math
 # ------------------------ 
 
 def normalisation(dataframe):
@@ -169,5 +172,115 @@ def CHA(DF, linkage='centroid', verbose=False, dendrogramme=False):
             # Affichage du résultat obtenu:
             plt.show()
         return l
+    
+def inertie_cluster(Ens):
+    return np.sum(dist_euclidienne(Ens , centroide(Ens))**2)
 
-    raise NotImplementedError("Please Implement this method")
+def init_kmeans(K,Ens):
+    return np.array(Ens.loc[np.random.choice(Ens.index , K , replace = False)])
+
+def dist_eucl(X , Y):
+    return np.sum((X-Y)**2 , axis=1)**0.5
+
+def plus_proche(Exe,Centres):
+    return np.argmin(dist_eucl(Centres , np.array(Exe)))
+
+def affecte_cluster(Base,Centres):
+    mat_affect = {i:[] for i in range(len(Centres))}
+    for i in range(len(Base)):
+        mat_affect[plus_proche(Base.iloc[i] , Centres)].append(i)
+    return mat_affect
+
+def nouveaux_centroides(Base,U):
+    new_centroid = []
+    for i in U.keys() :
+        new_centroid.append(centroide(Base.iloc[U[i]]))
+    return np.array(new_centroid)
+
+def inertie_globale(Base, U):
+    inertie_glob = 0
+    for i in U.keys():
+        inertie_glob += inertie_cluster(Base.iloc[U[i]])
+    return inertie_glob
+
+def kmoyennes(K, Base, epsilon, iter_max):
+    # initialisation des centroides
+    Centroides = init_kmeans(K, Base)
+
+    # initialisation du dictionnaire d'affectation
+    DictAffect = affecte_cluster(Base, Centroides)
+
+    # initialisation de l'inertie globale
+    inertie1 = inertie_globale(Base, DictAffect)
+
+    # affichage des informations
+    #print(f'iteration {1} Inertie : {inertie1:1.4f} Difference: {(inertie1 - epsilon - 1):1.4f}')
+
+    # initialisation du compteur d'itérations
+    compteur = 2
+
+    # boucle principale
+    while compteur <= iter_max:
+
+        # recalcul des nouveaux centroides
+        Centroides = nouveaux_centroides(Base, DictAffect)
+
+        # recalcul du dictionnaire d'affectation
+        DictAffect = affecte_cluster(Base, Centroides)
+
+        # recalcul de l'inertie globale
+        inertie2 = inertie_globale(Base, DictAffect)
+
+        # calcul de la différence d'inertie
+        diff = inertie1 - inertie2
+
+        # mise à jour de l'inertie globale
+        inertie1 = inertie2
+
+        # affichage des informations
+        #print(f'iteration {compteur} Inertie : {inertie1:1.4f} Difference: {diff:1.4f}')
+
+        # test de la condition d'arrêt
+        if diff < epsilon:
+            break
+        
+        # incrémentation du compteur
+        compteur += 1
+  
+    return Centroides, DictAffect
+
+def compacite_cluster(Ens):
+    max_dist = -math.inf
+    for i in range(0,len(Ens)):
+        dist = np.max(dist_eucl(np.array(Ens.iloc[i]),np.array(Ens)))
+        if (dist > max_dist):
+            max_dist =dist
+    return max_dist
+
+def compacite_globale(Base, U):
+    compacite_glob = 0
+    for i in U.keys():
+        compacite_glob += compacite_cluster(Base.iloc[U[i]])
+    return compacite_glob
+
+def semin(Base, U):
+    centroid = [centroide(np.array(Base.iloc[U[i]])) for i in U.keys()]
+    sep_min = math.inf
+    for i in range(len(centroid)-1):
+        sep = np.min(dist_eucl(centroid[i] , centroid[i+1 :] ))
+        if sep < sep_min:
+            sep_min = sep
+    return sep_min
+
+def index_Dunn(Base, U):
+    return compacite_globale(Base, U) / semin(Base, U)
+
+def index_Xie_Beni(Base, U):
+    return inertie_globale(Base, U) / semin(Base, U)
+
+def affiche_resultat(Base,Centres,Affect):
+    couleurs = cm.tab20(np.linspace(0, 1, len(Centres)))    
+
+    for i in range(len(Centres)):
+        plt.scatter(Base.iloc[Affect[i]]['X1'], Base.iloc[Affect[i]]['X2'], color=couleurs[i])
+    plt.scatter(Centres[:,0],Centres[:,1],color='r',marker='x')
